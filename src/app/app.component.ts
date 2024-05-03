90d2aa12f5a6a4f6303dbb35c876d17b117a01f4
AngeloCarnevale <angelo.carnevale@hotmail.com>
2024-05-03 16:49:36 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'weather-forecast';
+  data:any = {};
+  httpClient = inject(HttpClient);
+  api_key = '45937c74ec7cc2298a6fe8c7bb4417df';
+
+  searchWeatherForecast() {
+    const response = this.httpClient.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=Campinas&appid=${this.api_key}`
+    );
+    console.log(
+      response.subscribe((data: any) => {
+        this.data = data;
+        console.log(data.name);
+      })
+    );
+  }
 }
